bcf7d5ede9ff16e4455ef7f43b60add5d272a048
ErickSteveCA <20006291@uan.edu.mx>
2024-05-08 16:46:26 -0700

    Scripts Software Libre

---
+++ script03.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Autor: Erick Steve Contreras Ante
+#Fecha: 6-03-24
+#Descripcion: Script para manejo de parametros.
+echo "El nombre del script es: $0"
+echo "Los argumentos son: $1 - $2 $3"
---
+++ script04.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Autor: Erick Steve Contreras Ante
+#Fecha: 6-03-24
+function aqui { 
+	echo "mundo $1"
+}
+aqui $1
